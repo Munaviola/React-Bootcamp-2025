3f88415926a535d3a1ade1793311fc0cebe1c5bd
Munviola <munnaviola5@gmail.com>
2025-01-09 21:22:41 +0100

    day-08 commit

---
+++ daily-learning/January/day-08/exercise/day-08.js
@@ -0,0 +1,49 @@
+//string
+const animal = 'dog'
+const annonymous = 'John Doe'
+const nonLivingThing = 'bottle'
+const emptyString = ''
+const lastName = 'Echefu'
+const middleName = 'Viola'
+const firstName = 'Munachi'
+const musician = 'Rihannah'
+const capitalInNigeria = 'Owerri'
+const stateInNigeria = 'Imo State'
+
+//Number
+const one = 1
+const two = 2
+const three = 3
+const four = 4
+const five = 5
+const six = 6
+const seven = 7
+const eight = 8
+const nine = 9
+const ten = 10
+
+
+//boolean
+const darkComplexion = true;
+const isNigerian = true;
+const isMarried = false;
+const isSnowing = false;
+const isAgirl = true;
+const isWorking = true;
+const notCurious = false;
+const hateLies = true;
+const isEating = false;
+const notOnline = true;
+
+
+//undefined
+let userName;       
+let userEmail;      
+let abc;     
+let qpr;      
+let letteredNumbers;        
+let deliveryDate;   
+let producer;   
+let biggerMe;   
+let isAdmin;        
+let userId;   
