66eafb719ad0e8eb53aac6adbb58b0d9d7ef4e11
Munviola <munnaviola5@gmail.com>
2025-01-20 12:24:29 +0100

    day-17 commit

---
+++ daily-learning/January/day-17/exercise/day-17.js
@@ -0,0 +1,150 @@
+
+const arrowOne = () => console.log(`This is Arrow One`);
+arrowOne();
+
+const arrowTwo = () => console.log(`This is Arrow Two`);
+arrowTwo();
+
+const arrowThree = () => console.log(`This is Arrow Three`);
+arrowThree();
+
+const arrowFour = () => console.log(`This is Arrow Four`);
+arrowFour();
+
+const arrowFive = () => console.log(`This is Arrow Five`);
+arrowFive();
+
+const arrowSix = () => console.log(`This is Arrow Six`);
+arrowSix();
+
+const arrowSeven = () => console.log(`This is Arrow Seven`);
+arrowSeven();
+
+const arrowEight = () => console.log(`This is Arrow Eight`);
+arrowEight();
+
+const arrowNine = () => console.log(`This is Arrow Nine`);
+arrowNine();
+
+const arrowTen = () => console.log(`This is Arrow Ten`);
+arrowTen();
+
+const arrowEleven = () => console.log(`This is Arrow Eleven`);
+arrowEleven();
+
+const arrowTwelve = () => console.log(`This is Arrow Twelve`);
+arrowTwelve();
+
+const arrowThirteen = () => console.log(`This is Arrow Thirteen`);
+arrowThirteen();
+
+const arrowFourteen = () => console.log(`This is Arrow Fourteen`);
+arrowFourteen();
+
+const arrowFifteen = () => console.log(`This is Arrow Fifteen`);
+arrowFifteen();
+
+const arrowSixteen = () => console.log(`This is Arrow Sixteen`);
+arrowSixteen();
+
+const arrowSeventeen = () => console.log(`This is Arrow Seventeen`);
+arrowSeventeen();
+
+const arrowEighteen = () => console.log(`This is Arrow Eighteen`);
+arrowEighteen();
+
+const arrowNineteen = () => console.log(`This is Arrow Nineteen`);
+arrowNineteen();
+
+const arrowTwenty = () => console.log(`This is Arrow Twenty`);
+arrowTwenty();
+
+const arrowTwentyOne = () => console.log(`This is Arrow Twenty-One`);
+arrowTwentyOne();
+
+const arrowTwentyTwo = () => console.log(`This is Arrow Twenty-Two`);
+arrowTwentyTwo();
+
+const arrowTwentyThree = () => console.log(`This is Arrow Twenty-Three`);
+arrowTwentyThree();
+
+const arrowTwentyFour = () => console.log(`This is Arrow Twenty-Four`);
+arrowTwentyFour();
+
+const arrowTwentyFive = () => console.log(`This is Arrow Twenty-Five`);
+arrowTwentyFive();
+
+const arrowTwentySix = () => console.log(`This is Arrow Twenty-Six`);
+arrowTwentySix();
+
+const arrowTwentySeven = () => console.log(`This is Arrow Twenty-Seven`);
+arrowTwentySeven();
+
+const arrowTwentyEight = () => console.log(`This is Arrow Twenty-Eight`);
+arrowTwentyEight();
+
+const arrowTwentyNine = () => console.log(`This is Arrow Twenty-Nine`);
+arrowTwentyNine();
+
+const arrowThirty = () => console.log(`This is Arrow Thirty`);
+arrowThirty();
+
+const arrowThirtyOne = () => console.log(`This is Arrow Thirty-One`);
+arrowThirtyOne();
+
+const arrowThirtyTwo = () => console.log(`This is Arrow Thirty-Two`);
+arrowThirtyTwo();
+
+const arrowThirtyThree = () => console.log(`This is Arrow Thirty-Three`);
+arrowThirtyThree();
+
+const arrowThirtyFour = () => console.log(`This is Arrow Thirty-Four`);
+arrowThirtyFour();
+
+const arrowThirtyFive = () => console.log(`This is Arrow Thirty-Five`);
+arrowThirtyFive();
+
+const arrowThirtySix = () => console.log(`This is Arrow Thirty-Six`);
+arrowThirtySix();
+
+const arrowThirtySeven = () => console.log(`This is Arrow Thirty-Seven`);
+arrowThirtySeven();
+
+const arrowThirtyEight = () => console.log(`This is Arrow Thirty-Eight`);
+arrowThirtyEight();
+
+const arrowThirtyNine = () => console.log(`This is Arrow Thirty-Nine`);
+arrowThirtyNine();
+
+const arrowForty = () => console.log(`This is Arrow Forty`);
+arrowForty();
+
+const arrowFortyOne = () => console.log(`This is Arrow Forty-One`);
+arrowFortyOne();
+
+const arrowFortyTwo = () => console.log(`This is Arrow Forty-Two`);
+arrowFortyTwo();
+
+const arrowFortyThree = () => console.log(`This is Arrow Forty-Three`);
+arrowFortyThree();
+
+const arrowFortyFour = () => console.log(`This is Arrow Forty-Four`);
+arrowFortyFour();
+
+const arrowFortyFive = () => console.log(`This is Arrow Forty-Five`);
+arrowFortyFive();
+
+const arrowFortySix = () => console.log(`This is Arrow Forty-Six`);
+arrowFortySix();
+
+const arrowFortySeven = () => console.log(`This is Arrow Forty-Seven`);
+arrowFortySeven();
+
+const arrowFortyEight = () => console.log(`This is Arrow Forty-Eight`);
+arrowFortyEight();
+
+const arrowFortyNine = () => console.log(`This is Arrow Forty-Nine`);
+arrowFortyNine();
+
+const arrowFifty = () => console.log(`This is Arrow Fifty`);
+arrowFifty();
