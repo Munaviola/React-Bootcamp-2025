d7bbfee95c984aa40303f34e440be035b21a4e44
Munviola <munnaviola5@gmail.com>
2025-02-13 12:23:28 +0100

    day-32 commit

---
+++ daily-learning/February/day-32/exercise/day-32.js
@@ -0,0 +1,14 @@
+const exponentialValue = Math.exp(2);
+const logarithmicValue = Math.log(2);
+
+console.log(exponentialValue);
+console.log(logarithmicValue);
+
+console.log(Math.round(exponentialValue));
+console.log(Math.round(logarithmicValue));
+
+console.log(Math.ceil(exponentialValue));
+console.log(Math.ceil(logarithmicValue));
+
+console.log(Math.floor(exponentialValue));
+console.log(Math.floor(logarithmicValue));
