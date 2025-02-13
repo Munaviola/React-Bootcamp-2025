c1d09afc2ef9231907b47be9cd70273d488c47cb
Munviola <munnaviola5@gmail.com>
2025-02-13 19:29:22 +0100

    day-37 commit

---
+++ daily-learning/February/day-37/exercise/day-37.js
@@ -0,0 +1,29 @@
+// Print numbers from 1-10 using a for loop
+// for ( let i = 1; i <= 10; i++){
+//   console.log(i)
+// }
+
+// Print numbers from 1-10 using a while loop
+// let j = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// Print numbers from 1-10 using a do...while loop
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++
+// }while (i <= 10);
+
+// Write a program that prints even numbers from 2-20 usind a for loop
+
+// for (let i = 2; i <= 20; i +=2) {
+//   console.log(i);
+// }
+
+// given an object 
+
+
